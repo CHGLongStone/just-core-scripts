e250d595fd938a0d9a697f757755f0f0491b82cf
CHGLongStone <jason.medland@gmail.com>
2016-04-12 20:42:14 -0400

    big clean up

--- SchemaSyncWrapper.sh
+++ SchemaSyncWrapper.sh
@@ -2,7 +2,7 @@
 # SchemaSyncWrapper (version 1)
 # Jason Medland <jason.medland@gmail.com>
 ##########################################
-# dependancy:
+# dependency:
 #	https://github.com/mmatuson/SchemaSync
 #	
 #
@@ -21,6 +21,11 @@ OUTPUT_PATH="/var/www/vhosts/blackwatch_dev/data/updates/"
 #######################################
 source "$DIR/"management_utils.sh
 
+SKIP="yes"
+#######################################
+# whoami 
+#######################################
+echo  -e "${CYAN} ${0##*/} ${NC}";
 #source "$DIR/"db_config.sh
 
 function usage(){
@@ -33,8 +38,10 @@ function usage(){
 	- create a branch for the user
 	- set vhost configuration
 	
-	\033[1m OPTIONS:\033[0m 
+	 OPTIONS:
 	\033[1m	-h \033[0m     Show this message
+	\033[1m	-m \033[0m     passthrough arg --tag=
+	\033[1m	-d \033[0m     passthrough arg --output-directory=
 	\033[1m	-l \033[0m     log directory 
 							(local $DIR  if not otherwise specified)
 	\033[1m	-r \033[0m     redirect 
@@ -56,18 +63,31 @@ function usage(){
 
 vflag=
 tflag=
-
+declare -A OPTS_MAPPING
+#
+OPTS_MAPPING["v"]="verbose"
+OPTS_MAPPING["d"]="output Directory"
+OPTS_MAPPING["l"]="Log directory"
+OPTS_MAPPING["n"]="tag Name"
+OPTS_MAPPING["r"]="verbose"
+OPTS_MAPPING["s"]="source db"
+OPTS_MAPPING["t"]="target db"
+OPTS_MAPPING["h"]="usage"
 
 #echo -e ${BLUE}"$0 $#args[$@] OPTIND[$OPTIND] OPTARG[$OPTARG]"${NC}
-while getopts ":v:l:r:s:t:h" name; do
-	echo -e "		${purple}FLAG:" $name "VALUE: $OPTARG ${NC}"
-	#execution_string=$execution_string" -"$name" $OPTARG"
+while getopts ":v:d:n:l:r:s:t:h" name; do
+	#echo -e "		${purple} $name: [" ${OPTS_MAPPING[$name]}  "]: $OPTARG ${NC}"
+	
 	case $name in
 		v)  vflag=1
 			VERBOSE_OUT="$OPTARG"
 			vval="$OPTARG";;	
+		d)  dflag=1
+			dval="$OPTARG";;
 		l)  lflag=1
 			lval="$OPTARG";;
+		n)  mflag=1
+			mval="$OPTARG";;
 		r)  rflag=1
 			rval="$OPTARG";;
 		s)  sflag=1
@@ -81,34 +101,60 @@ done
 
 
 
-#######################################
-# validate params
-#
-# set the log dir 
-#######################################
-if [ ! -z "$lflag" ]; then
-	LOG_DIR=$lval
-	echo -e "${purple} LOG_DIR SET:" $lval " ${NC}"
-else
-	LOG_DIR=$DIR
-	echo -e "${purple}NO LOG_DIR SET using :" $DIR " ${NC}"
-fi
 
 #######################################
 # check the redirect flag (copy the sql files to repo)
+# deprecate this redirect and use the flags
 #######################################
-if [ ! -z "$rflag" ] ; then
-	echo -e "${GREEN}redirect: prefix $rval  to $OUTPUT_PATH ${NC}"
-else
-	echo -e "${RED}NO redirect: $rval writing files to local dir ${NC}"
+#if [ ! -z "$rflag" ] ; then
+#	echo -e "${GREEN}redirect: prefix $rval  to $OUTPUT_PATH ${NC}"
+#else
+#	echo -e "${RED}NO redirect: $rval writing files to local dir ${NC}"
 	#exit 1;
-fi
+#fi
 
 #######################################
-# set the soruce DB
+# do some basic path setting
+#	default OUTPUT_PATH is hard coded to this project
+#	if we're using the -d PASSTHROUGH arg we'll set the OUTPUT_PATH_FINAL
+#	
+#	
+#	
+#######################################
+
+if [ -z "$dval" ] ; then
+	echo -e "${RED}output_directory: $dval  MISSING ${NC}"
+	OUTPUT_PATH_FINAL=$OUTPUT_PATH
+else
+	#echo -e "${GREEN}output_directory: $dval  SET ${NC}"
+	OUTPUT_PATH_FINAL=$dval
+fi
+
+
+#######################################
+# validate params
+# set the log dir, by default the just-core-scripts dir
+#
+#######################################
+if [ ! -z "$lflag" ]; then
+	LOG_DIR=$lval
+	#echo -e "${purple} LOG_DIR SET:" $lval " ${NC}"
+else
+	LOG_DIR=$DIR
+	if [ ! -z "$vflag" ]; then
+		echo -e "${purple}	NO LOG_DIR SET using :" $DIR " ${NC}"
+	fi
+	
+fi
+
+
+
+#######################################
+# set the source DB
 #######################################
 if [ ! -z "$sflag" ] ; then
-	echo -e "${GREEN}source: $sval  FOUND ${NC}"
+	#echo -e "${GREEN}source: $sval  FOUND ${NC}"
+	SKIP="yes"
 else
 	echo -e "${RED}source: $sval  MISSING ${NC}"
 	exit 1;
@@ -119,11 +165,42 @@ fi
 # set the target DB
 #######################################
 if [ ! -z "$tflag" ]; then
-	echo -e "${GREEN}target: $tval  FOUND ${NC}"
+	SKIP="yes"
+	#echo -e "${GREEN}target: $tval  FOUND ${NC}"
 else
 	echo -e "${RED}target: $tval  MISSING ${NC}"
 	exit 1;
 fi
+
+#######################################
+# PASSTHROUGH ARGS
+#	--output-directory
+#	--tag
+#	
+#######################################
+output_directory="--output-directory=$dval"
+if [ ! -z "$dflag" ]; then
+	#$output_directory=` --output-directory=$dval `
+	SKIP="yes"
+	#echo -e "${GREEN} output_directory "$output_directory"  FOUND ${NC}"
+else
+	$output_directory=""
+	echo -e "${RED}output_directory $dval  MISSING ${NC}"
+	exit 1;
+fi
+
+tag_output="--tag=$mval"
+if [ ! -z "$mflag" ]; then
+	SKIP="yes"
+	#echo -e "${GREEN}tag_output "$tag_output"  FOUND ${NC}"
+else
+	$tag_output=""
+	echo -e "${RED} tag_output $mval  MISSING ${NC}"
+	exit 1;
+fi
+
+
+
 
 
 #######################################
@@ -144,7 +221,7 @@ file="$DIR/db_config.$sval"
 if [ -f $file ]; then 
 	source $file
 	sync_source="mysql://${hostdata[USER]}:${hostdata[PASS]}@${hostdata[SERVER]}:${hostdata[PORT]}/${hostdata[DATABASE]}"
-	echo -e "${GREEN}sync_source ${hostdata[SERVER]}.${hostdata[DATABASE]} ${NC}"
+	#echo -e "${GREEN}sync_source ${hostdata[SERVER]}.${hostdata[DATABASE]} ${NC}"
 else
 	echo -e "${RED}failed to load target db_config.$sval ${NC}"
 fi
@@ -156,55 +233,64 @@ file="$DIR/db_config.$tval"
 if [ -f $file ]; then 
 	source $file
 	sync_target="mysql://${hostdata[USER]}:${hostdata[PASS]}@${hostdata[SERVER]}:${hostdata[PORT]}/${hostdata[DATABASE]}"
-	echo -e "${GREEN}sync_target ${hostdata[SERVER]}.${hostdata[DATABASE]} ${NC}"
+	#echo -e "${GREEN}sync_target ${hostdata[SERVER]}.${hostdata[DATABASE]} ${NC}"
 else
 	echo -e "${RED}failed to load target db_config.$tval ${NC}"
 	
 fi
-
+#######################################
 #	schemasync [options] <source> <target>
 #	source/target format: mysql://user:pass@host:port/database
-
-schemasync $sync_source $sync_target 2> /dev/null
+# http://mmatuson.github.io/SchemaSync/
+schemasync $output_directory $tag_output $sync_source $sync_target 2> /dev/null
 ec=$?
-
+#######################################
+#######################################
 #
-# If there is an error we could not connect. The erro will be in the log file.
+# If there is an error we could not connect. The error will be in the log file.
 #
+#######################################
 if [ "$ec" -ne 0 ]; then
-   cat $LOG_DIR/schemasync.log
+   cat $OUTPUT_PATH_FINAL/schemasync.log
    exit $ec;
 fi
 
-#
-# If we are here we will look for the schema scripts. If they exist then 
+#######################################
+# If we are here we will look for the schema scripts. If they exist then ${VERSION//.}
 # the schemas were not the same.
 #
-set=`chmod 1755 ${hostdata[DATABASE]}.*.sql 2> /dev/null`
-patch=`ls ${hostdata[DATABASE]}.*.patch.sql 2> /dev/null`
-revert=`ls ${hostdata[DATABASE]}.*.revert.sql 2> /dev/null`
+#echo -e "${CYAN}revert: $OUTPUT_PATH_FINAL/${hostdata[DATABASE]}_${mval//.}.*.patch.sql ${NC}"
+#######################################
+set=`chmod 1755 $OUTPUT_PATH_FINAL/${hostdata[DATABASE]}_*.sql 2> /dev/null`
+patch=`ls  $OUTPUT_PATH_FINAL/${hostdata[DATABASE]}_${mval//.}.*.patch.sql 2> /dev/null`
+revert=`ls $OUTPUT_PATH_FINAL/${hostdata[DATABASE]}_${mval//.}.*.revert.sql 2> /dev/null`
 
+#echo -e "${CYAN}revert: $OUTPUT_PATH_FINAL/${hostdata[DATABASE]}.*.sql ${NC}"
 
+#echo -e "${CYAN}patch:  $patch  ${NC}"
+#echo -e "${CYAN}revert:  $revert  ${NC}"
+#######################################
+# if a patch was created there is a schema difference
+# 
+#######################################
 if [ -f "${patch}" ]; then
-   echo "File $patch created. Check into Source Control";
-	if [ ! -z "$rflag" ] ; then
-		echo -e "${GREEN}redirect:  $DIR/${patch} $OUTPUT_PATH$rval.patch.sql${NC}"
-		cp -R "$DIR/${patch}"  "$OUTPUT_PATH$rval.patch.sql"
-
-		echo -e "${GREEN} ${revert}                 $OUTPUT_PATH$rval.revert.sql ${NC}"
-		cp -R "$DIR/${revert}"  "$OUTPUT_PATH$rval.revert.sql"
-		#echo -e "${GREEN}${revert}                  $OUTPUT_PATH$rval.revert.sql ${NC}"
-		
-	else
-		echo -e "${RED}redirect: $OUTPUT_PATH$rval  MISSING ${NC}"
-		exit 1
-	fi
+	#patch_new="$OUTPUT_PATH_FINAL/$mval.patch.sql"
+	patch_result=` cp -R ${patch}	$OUTPUT_PATH_FINAL/$mval.patch.sql`
+	revert_result=`cp -R ${revert}	$OUTPUT_PATH_FINAL/$mval.revert.sql`
+	#echo -e "${YELLOW} REVERT: ${revert} $OUTPUT_PATH_FINAL/$mval.revert.sql ${NC}"
+	set=`chmod 1755 $OUTPUT_PATH_FINAL/${hostdata[DATABASE]}_*.sql 2> /dev/null`
    
+	echo -e "${CYAN}";
+	echo	"	Files created. ";
+	echo	"	Check into Source Control ";
+	echo 	"	$OUTPUT_PATH_FINAL/$mval.patch.sql ";
+	echo 	"	$OUTPUT_PATH_FINAL/$mval.revert.sql ";
+	echo -e "${NC}";
    
-   
-   
+	# clean up temp files
+	rm ${patch} ${revert}
 else
    echo "Schemas identical";
-   rm $LOG_DIR/schemasync.log
+   rm $OUTPUT_PATH_FINAL/schemasync.log
 fi
 exit 0;
