e94ab60e5ff53b380e0159c052d84bba689c4123
CHGLongStone <jason.medland@gmail.com>
2017-11-07 14:13:07 -0500

    update script for new env

--- update_production.sh
+++ update_production.sh
@@ -36,9 +36,10 @@ function usage(){
 
 	${YELLOW} OPTIONS:${NC}
 	${YELLOW}	-h ${yellow}     Show this message
+	${YELLOW}	-e ${yellow}     environment 
 	${YELLOW}	-t ${yellow}     tag name
 	${YELLOW}	-m ${yellow}     execute MySQL update in v#.data.sql [Y]
-	
+	${NC}
 	"
 }
 
@@ -60,7 +61,7 @@ while getopts ":v:e:t:m:h" name; do
 			VERBOSE_OUT="$OPTARG"
 			vval="$OPTARG";;	
 		e)  eflag=1
-			eval="$OPTARG";;
+			enval="$OPTARG";;
 		t)  tflag=1
 			tval="$OPTARG";;
 		m)  mflag=1
@@ -85,16 +86,44 @@ fi
 #	* checking out the release tag into directory `[project_name]_release/[release_tag]`
 
 #$checkout_dir #env_config.sh release_dir
-if [ ! -z "$eflag" ] && [ "uat" == $eval ]; then
-	echo -e "${green}Using ENVIRONMENT  ${NC}$eval "
-	checkout_dir=$candidate_dir
-	http_serve_path=$uat_dir
+if [ ! -z "$eflag" ] ; then  #&& [ "uat" == $enval ]
+	
+	echo -e "${green}Using ENVIRONMENT  ${NC}$enval "
+	test_sym_link="{$enval[\"sym_link\"]}"
+	#echo -e "${green}sym_link  "${sevenV["sym_link"]}" ${NC}eval $enval  test_sym_link $test_sym_link"
+	#sym_link="$"$test_sym_link
+	eval sym_link=\$$test_sym_link
+	#echo -e "${green}-- sym_link  $sym_link "${sym_link}"  test_sym_link "${test_sym_link}"  ${NC}"
+	if [ -d $sym_link ] ; then
+		echo -e "${green} sym_link set  ${NC}$sym_link "
+		http_serve_path=$sym_link		
+	fi
+	
+	test_app_dir="{$enval[\"app_dir\"]}"
+	eval app_dir=\$$test_app_dir
+	#echo -e "${green}app_dir  "${sevenV["app_dir"]}" ${NC}eval $enval  test_app_dir $test_app_dir"
+	if [ -d $app_dir ] ; then
+		echo -e "${green} app_dir set  ${NC}$app_dir "
+		checkout_dir=$app_dir
+	fi
+	
+	
 else
-	echo -e "${green}Using ENVIRONMENT  ${NC} PROD "
-	checkout_dir=$release_dir
-	http_serve_path=$prod_dir
+	echo -e "${RED}NO  ENVIRONMENT  ${NC} "
+	#checkout_dir=$release_dir
+	#http_serve_path=$prod_dir
+	exit;
 fi
-echo -e "${green}checkout_dir[$checkout_dir] http_serve_path[$http_serve_path] ${NC} PROD "
+#echo -e "${green}checkout_dir[$checkout_dir] http_serve_path[$http_serve_path] ${NC} PROD "
+
+
+echo -e "${RED}DIE HERE  ${NC} PROD "
+
+
+
+
+
+
 
 #######################################
 #check out the project
@@ -177,11 +206,11 @@ git clone  git@github.com:CHGLongStone/blackwatch.wiki.git blackwatch.wiki
 #######################################
 if [ ! -z "$mflag" ] && [ "$mval" == "Y" ]; then
 	#file="$checkout_dir/$tval/data/updates/$tval.schema.sql"
-	#file="$checkout_dir/$tval/data/updates/$tval"_"$eval.patch.sql" 
+	#file="$checkout_dir/$tval/data/updates/$tval"_"$enval.patch.sql" 
 	#v1.0.3 	/v1.0.3.patch.uat.from.dev.sql
-	file="$checkout_dir/$tval/data/updates/$tval.patch.$eval.from.dev.sql"
-	echo -e "${green}EXECUTING SQL UPDATE WTIH ${NC} $file"   
-	[ -f $file ] && SQLUPDATE=`mysql --defaults-file=$DIR/lib/chglongstone/mysql-db-sync/my.prod.cnf <  "$file" 2>&1;`
+	file="$checkout_dir/$tval/data/updates/$tval.patch.$enval.from.dev.sql"
+	echo -e "${green}EXECUTING SQL UPDATE WTIH ${NC} my.$enval.cnf $file"   
+	[ -f $file ] && SQLUPDATE=`mysql --defaults-file=$DIR/lib/chglongstone/mysql-db-sync/my.$enval.cnf <  "$file" 2>&1;`
 	echo -e "${green}UPDATE RESULT ${NC} $SQLUPDATE"   
 	SQLUPDATE=""
 else
@@ -190,9 +219,9 @@ fi
 
 
 if [ ! -z "$mflag" ] && [ "$mval" == "Y" ]; then
-	file=" $checkout_dir/$tval/data/updates/$tval.data.$eval.from.dev.sql"
-	echo -e "${green}EXECUTING SQL UPDATE WTIH ${NC} $file"   
-	[ -f $file ] && SQLUPDATE=`mysql --defaults-file=$DIR/lib/chglongstone/mysql-db-sync/my.prod.cnf <  "$file" 2>&1;`
+	file=" $checkout_dir/$tval/data/updates/$tval.data.$enval.from.dev.sql"
+	echo -e "${green}EXECUTING SQL UPDATE WTIH ${NC} my.$enval.cnf $file"   
+	[ -f $file ] && SQLUPDATE=`mysql --defaults-file=$DIR/lib/chglongstone/mysql-db-sync/my.$enval.cnf <  "$file" 2>&1;`
 	echo -e "${green}UPDATE RESULT ${NC} $SQLUPDATE"   
 	SQLUPDATE=""
 	
